20daf3c9e786ffb0146ae069540f1092afcad059
Matias Hermanrud Fjeld <mhf@hex.no>
2015-04-17 13:58:37 +0200

    Add Box to physics world when connected to scene

--- Box.cpp
+++ Box.cpp
@@ -6,8 +6,17 @@ Box::Box(qreal x, qreal y, qreal width, qreal height) :
   // Interpret x and y arguments as the *top left corner of the box*,
   // not the center.
   QGraphicsRectItem{x - width / 2.0f, y - height / 2.0f, width, height},
+  body{nullptr}
 {
   setBrush(QBrush{Qt::white});
+}
+
+Box::~Box() {
+  delete fixtureDef.shape;
+  fixtureDef.shape = nullptr;
+  // TODO: Delete body from world?
+  //gameScene()->world()->DestroyBody(body);
+  //body = nullptr;
 }
 
 void Box::advance(int phase) {
@@ -16,13 +25,26 @@ void Box::advance(int phase) {
 
   if (phase == 0) return;
 
-  // FIXME: Obtain position from physics world
+  if (body != nullptr and body->IsAwake())
+    setPos(gameScene()->mapFromWorld(body->GetPosition()));
 }
 
 QVariant Box::itemChange(GraphicsItemChange change, const QVariant & value) {
   // TODO: Handle item being removed from scene
   if (change == QGraphicsItem::ItemSceneHasChanged) {
-    // FIXME: Add to new scene world
+    b2PolygonShape* shape = new b2PolygonShape;
+    b2Vec2 vec = gameScene()->mapToWorld(sceneBoundingRect().bottomRight());
+    shape->SetAsBox(vec.x, vec.y);
+    fixtureDef.density = 1;
+    fixtureDef.restitution = 0.5f;
+    fixtureDef.shape = shape;
+
+    b2BodyDef bodyDef;
+    bodyDef.userData = reinterpret_cast<void*>(this);
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position = gameScene()->mapToWorld(scenePos());
+    body = gameScene()->world()->CreateBody(&bodyDef); // Copies content of bodyDef
+    body->CreateFixture(&fixtureDef); // FIXME: Check that this doesn't leak memory
   }
   return value;
 }
--- Box.hpp
+++ Box.hpp
@@ -1,12 +1,21 @@
 #ifndef BOX_HPP
 #define BOX_HPP
 
+#include "GameScene.hpp"
+
 #include <QGraphicsRectItem>
 #include <QVariant>
+#include <Box2D.h>
 
 class Box : public QGraphicsRectItem {
+private:
+  b2FixtureDef fixtureDef;
+  b2Body* body;
+
+  inline GameScene* gameScene() {return dynamic_cast<GameScene*>(scene());}
 public:
   Box(qreal x, qreal y, qreal width, qreal height);
+  ~Box();
 protected slots:
   void advance(int phase) override;
   QVariant itemChange(GraphicsItemChange change, const QVariant & value);
