da18b30047aec0a3cbb55200b34fd79555fd8816
Matias Hermanrud Fjeld <mhf@hex.no>
2015-04-20 15:13:55 +0200

    Added test for correct interpretation of Box coordinates

--- tests/BoxTest.cpp
+++ tests/BoxTest.cpp
@@ -5,6 +5,15 @@
 
 class BoxTest : public GameSceneTest {};
 
-TEST_F(BoxTest, addBox) {
-  scene.addItem(new Box{0, 0, scene.width(), scene.height()});
+TEST_F(BoxTest, interpret_coordinates_as_position_of_top_left_corner) {
+  // Create a box that fills the entire scene
+  Box* box = new Box{0, 0, scene.width(), scene.height()};
+  scene.addItem(box);
+
+  // The coordinates given to the box constructor should be
+  // interpreted as the x and y coordinates of the top left corner of
+  // the box. After creating the box, the x() and y() parameters
+  // should return the position of the center of the box.
+  EXPECT_DOUBLE_EQ(scene.width() / 2.0, box->x());
+  EXPECT_DOUBLE_EQ(scene.height() / 2.0, box->y());
 }
