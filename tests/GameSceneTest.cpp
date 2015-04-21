4f77dee744f11675e14a1317980c5e412fa770b6
Matias Hermanrud Fjeld <mhf@hex.no>
2015-04-21 11:36:41 +0200

    Test that the world has a ground

--- tests/GameSceneTest.cpp
+++ tests/GameSceneTest.cpp
@@ -2,7 +2,17 @@
 
 #include "GameSceneTest.hpp"
 
+#include "Box.hpp"
+
 TEST_F(GameSceneTest, world_has_same_origin_as_scene) {
-  EXPECT_DOUBLE_EQ(scene.mapToWorld(0), 0);
-  EXPECT_DOUBLE_EQ(scene.mapFromWorld(0), 0);
+  EXPECT_DOUBLE_EQ(0, scene.mapToWorld(0));
+  EXPECT_DOUBLE_EQ(0, scene.mapFromWorld(0));
+}
+
+TEST_F(GameSceneTest, world_has_ground) {
+  Box* box = new Box{0, 0, scene.width(), scene.height()};
+  scene.addItem(box);
+  double originalVerticalPosition = box->y();
+  scene.advance(1000.0d); // milliseconds
+  EXPECT_DOUBLE_EQ(originalVerticalPosition, box->y());
 }
