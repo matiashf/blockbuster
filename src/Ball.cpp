5f219fcacd6fcc4ce4826b48bdf2d4e619bd1a9d
Matias Hermanrud Fjeld <mhf@hex.no>
2015-04-22 16:38:26 +0200

    Connected Ball with physics world

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -4,6 +4,8 @@
 
 Ball::Ball(qreal x, qreal y, qreal radius) :
   QGraphicsEllipseItem{-radius, -radius, radius * 2, radius * 2},
+  body{nullptr},
+  impulseVector{0.0d, 0.0d}
 {
   setBrush(QBrush{Qt::white});
   setPos(x + radius, y + radius);
@@ -15,5 +17,28 @@ void Ball::advance(int phase) {
 
   if (phase == 0) return;
 
-  moveBy(1, 1);
+  if (body != nullptr and body->IsAwake()) {
+    setPos(gameScene()->mapFromWorld(body->GetPosition()));
+    // TODO: Apply rotation
+  }
+}
+
+QVariant Ball::itemChange(GraphicsItemChange change, const QVariant & value) {
+  if (change == QGraphicsItem::ItemSceneHasChanged) {
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.bullet = true; // Prevent the ball from ever passing through another object, at the cost of more expensive collision calculations.
+    bodyDef.position = gameScene()->mapToWorld(scenePos());
+    body = gameScene()->world()->CreateBody(&bodyDef); // Copies content of bodyDef
+
+    b2CircleShape shape;
+    shape.m_p.Set(0, 0); // Draw the center of the circle at the body origin
+    shape.m_radius = gameScene()->mapToWorld(radius());
+    b2FixtureDef fixtureDef;
+    fixtureDef.density = 1.0f;
+    fixtureDef.restitution = 0.5f;
+    fixtureDef.shape = &shape;
+    body->CreateFixture(&fixtureDef); // Deep copies fixtureDef.
+  }
+  return value;
 }
--- src/Ball.hpp
+++ src/Ball.hpp
@@ -2,12 +2,18 @@
 #define BALL_HPP
 
 #include <QGraphicsEllipseItem>
+#include <Box2D.h>
 
 class Ball : public QGraphicsEllipseItem {
+private:
+  b2Body* body;
+  inline GameScene* gameScene() {return dynamic_cast<GameScene*>(scene());}
 public:
   Ball(qreal x, qreal y, qreal radius);
+  inline qreal radius() { return rect().width() / 2.0d; };
 protected slots:
   void advance(int phase) override;
+  QVariant itemChange(GraphicsItemChange change, const QVariant & value);
 };
 
 #endif
