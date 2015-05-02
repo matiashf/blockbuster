11ab625ea0820d9a3ef7c861a70e889ee8dd9068
Matias Hermanrud Fjeld <mhf@hex.no>
2015-05-02 19:01:43 +0200

    Allow smooth keyboard control by doing manual key repeats in KeyboardPlayer

--- src/KeyboardPlayer.cpp
+++ src/KeyboardPlayer.cpp
@@ -2,37 +2,62 @@
 
 #include <QKeyEvent>
 
+// We're not concerned about avoiding multiple repetitive control
+// actions on a single keypress, but about how the human player
+// perceives the responsiveness of keyboard interaction. Thus the time
+// between repeated actions value is very low, with the timer firing
+// once for every every second frame of the game. Qt also aligns
+// timers, so this will most likely be attached to the 60 Hz frame
+// timer.
+const int KeyboardPlayer::kMillisecondsBetweenRepetitions = 33; // results in 30Hz
+
 KeyboardPlayer::KeyboardPlayer(GameScene* scene, Ball* ball, Qt::Key up,
                                Qt::Key down, Qt::Key left, Qt::Key right,
                                Qt::Key apply) :
-  Player{scene, ball},
-  up_key{up},
-  down_key{down},
-  left_key{left},
-  right_key{right},
-  apply_key{apply}
+  Player{scene, ball}
 {
+  registerKey(up, &KeyboardPlayer::increaseImpulse);
+  registerKey(down, &KeyboardPlayer::decreaseImpulse);
+  registerKey(left, &KeyboardPlayer::rotateLeft);
+  registerKey(right, &KeyboardPlayer::rotateRight);
+  registerKey(apply, &KeyboardPlayer::applyImpulse);
 }
 
 bool KeyboardPlayer::eventFilter(QObject * watched, QEvent * event) {
   Q_UNUSED(watched);
-  if (event->type() == QEvent::KeyPress) {
+
+  if (event->type() == QEvent::KeyPress || event->type() == QEvent::KeyRelease) {
     QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
-    int key = keyEvent->key();
-    if (key == up_key)
-      increaseImpulse();
-    else if (key == down_key)
-      decreaseImpulse();
-    else if (key == left_key)
-      rotateLeft();
-    else if (key == right_key)
-      rotateRight();
-    else if (key == apply_key)
-      applyImpulse();
-    else
-      return false;  // Continue processing the event
+
+    if (keyEvent->isAutoRepeat())
+      // Our own repeat timer for the key is already running.
+      return true;  // Discard the event.
+
+    auto iterator = timers.find(keyEvent->key());
+    if (iterator == timers.end())
+      return false;  // Not our key. Let someone else handle it.
+    QTimer* timer = iterator->second; // the maps stores a (key, timer*) pair
+
+    if (event->type() == QEvent::KeyPress) {
+      timer->start();
+      // Fire the timer right away, bypassing the protected
+      // restriction on timeout().  A better alternative would be to
+      // subclass QTimer to make an ImmediateTimer with an overridden
+      // start() method.
+      QMetaObject::invokeMethod(timer, "timeout"); // timer->timeout()
+    } else { // (event->type() == QEvent::KeyRelease)
+      timer->stop();
+    }
+
     return true;  // Stop processing the event (because it was handled here)
   }
 
   return false;  // Continue processing the event
+}
+
+void KeyboardPlayer::registerKey(Qt::Key key, Action action) {
+  QTimer* timer = new QTimer(this);
+  timer->setInterval(kMillisecondsBetweenRepetitions);
+  connect(timer, &QTimer::timeout, this, action);
+  timers.emplace(key, timer);
 }
--- src/KeyboardPlayer.hpp
+++ src/KeyboardPlayer.hpp
@@ -3,18 +3,36 @@
 
 #include "Player.hpp"
 
+#include <QTimer>
+#include <map> // std::map
+#include <functional> // std::function
+
+/* Control the ball using the keyboard.
+
+   This class uses QTimers to smooth out key repetition. Otherwise,
+   holding down e.g. the left key would result in a left rotation
+   followed by a delay before additional left rotations at an
+   unspecified interval. */
 class KeyboardPlayer : public Player {
   Q_OBJECT
 public:
   KeyboardPlayer(GameScene* scene, Ball* ball, Qt::Key up, Qt::Key down,
                  Qt::Key left, Qt::Key right, Qt::Key apply);
+
+  // Gets called by the scene when e.g. a keyEvent occurs
   virtual bool eventFilter(QObject * watched, QEvent * event);
 private:
-  Qt::Key up_key;
-  Qt::Key down_key;
-  Qt::Key left_key;
-  Qt::Key right_key;
-  Qt::Key apply_key;
+  // We need to store Qt::Key and look up int (from QKeyEvent::key),
+  // thus we use int not Qt::Key for the key type
+  std::map<int, QTimer*> timers;
+
+  static const int kMillisecondsBetweenRepetitions;
+
+  // Define Action to be a pointer to a member function of Player
+  typedef void (Player::*Action)();
+
+  // Sets up a timer and makes eventFilter recognize the key
+  void registerKey(Qt::Key, Action);
 };
 
 #endif
